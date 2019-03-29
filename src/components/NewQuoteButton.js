35ae74fe1cc42081cbfab3cc7b9480408b6a2da0
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-03-29 10:59:37 -0500

    First commit

---
+++ src/components/NewQuoteButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const button = styled.button`
+  background-color: orange;
+`;
+
+
+const NewQuoteButton = () => {
+  <button>Hit Me</button>
+}
