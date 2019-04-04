0231a75c3332e5ceaa52f359f10feb919f49e211
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-04-04 11:50:16 -0500

    Add quotes

--- src/common/quoteList.js
+++ src/common/quoteList.js
@@ -6,7 +6,191 @@ const quoteList = [
   {
     statement: "Potato potato potato potato",
     speaker: "--Harley"
-  }
+  },
+  {
+    statement: "If I were two-faced, would I be wearing this one?",
+    speaker: "--Abraham Lincoln"
+  },
+  {
+    statement: "The best thing about the future is that it comes one day at a time.",
+    speaker: "--Abraham Lincoln"
+  },
+  {
+    statement: "If there is anything the nonconformist hates worse than a conformist, it’s another nonconformist who doesn’t conform to the prevailing standard of nonconformity.",
+    speaker: "--Bill Vaughan"
+  },
+  {
+    statement: "Before you judge a man, walk a mile in his shoes. After that who cares?… He’s a mile away and you’ve got his shoes!",
+    speaker: "--Billy Connolly"
+  },
+  {
+    statement: "A bank is a place that will lend you money if you can prove that you don’t need it.",
+    speaker: "--"
+  },
+  {
+    statement: "As long as people will accept crap, it will be financially profitable to dispense it.",
+    speaker: "--Dick Cavett"
+  },
+  {
+    statement: "The cure for boredom is curiosity. There is no cure for curiosity.",
+    speaker: "--Dorothy Parker"
+  },
+  {
+    statement: "Never doubt the courage of the French. They were the ones who discovered that snails are edible.",
+    speaker: "--Doug Larson"
+  },
+  {
+    statement: "Human beings, who are almost unique in having the ability to learn from the experience of others, are also remarkable for their apparent disinclination to do so.",
+    speaker: "--Douglas Adams"
+  },
+  {
+    statement: "I refuse to answer that question on the grounds that I don’t know the answer.",
+    speaker: "--Douglas Adams"
+  },
+  {
+    statement: "Analyzing humor is like dissecting a frog. Few people are interested and the frog dies of it.",
+    speaker: "--E. B. White"
+  },
+  {
+    statement: "A computer once beat me at chess, but it was no match for me at kick boxing.",
+    speaker: "--Emo Philips"
+  },
+  {
+    statement: "How many people here have telekenetic powers? Raise my hand.",
+    speaker: "--Emo Philips"
+  },
+  {
+    statement: "Good advice is something a man gives when he is too old to set a bad example.",
+    speaker: "--Francois de La Rochefoucauld"
+  },
+  {
+    statement: "We learn from experience that men never learn anything from experience.",
+    speaker: "--George Bernard Shaw"
+  },
+  {
+    statement: "Happiness is having a large, loving, caring, close-knit family in another city.”",
+    speaker: "--George Burns"
+  },
+  {
+    statement: "May the forces of evil become confused on the way to your house.",
+    speaker: "--George Carlin"
+  },
+  {
+    statement: "Whoever said money can’t buy happiness didn’t know where to shop.",
+    speaker: "--Gertrude Stein"
+  },
+  {
+    statement: "I refuse to join any club that would have me as a member.",
+    speaker: "--Groucho Marx"
+  },
+  {
+    statement: "If you find it hard to laugh at yourself, I would be happy to do it for you.",
+    speaker: "--Groucho Marx"
+  },
+  {
+    statement: "Flattery is like cologne water, to be smelt, not swallowed.",
+    speaker: "--Josh Billings"
+  },
+  {
+    statement: "True terror is to wake up one morning and discover that your high school class is running the country.",
+    speaker: "--Kurt Vonnegut"
+  },
+  {
+    statement: "Originality is the fine art of remembering what you hear but forgetting where you heard it.",
+    speaker: "--Laurence J. Peter"
+  },
+  {
+    statement: "“I always wanted to be somebody, but now I realize I should have been more specific.",
+    speaker: "--Lily Tomlin"
+  },
+  {
+    statement: "The road to success is always under construction.",
+    speaker: "--Lily Tomlin"
+  },
+  {
+    statement: "I’m not for everyone. I’m barely for me.",
+    speaker: "--Marc Maron"
+  },
+  {
+    statement: "I am an old man and have known a great many troubles, but most of them never happened.",
+    speaker: "--Mark Twain"
+  },
+  {
+    statement: "I am only human, although I regret it.",
+    speaker: "--Mark Twain"
+  },
+  {
+    statement: "I would have written a shorter letter, but I did not have the time.",
+    speaker: "--Mark Twain"
+  },
+  {
+    statement: "The only way to keep your health is to eat what you don’t want, drink what you don’t like, and do what you’d rather not.",
+    speaker: "--Mark Twain"
+  },
+  {
+    statement: "Worrying is like paying a debt you don’t owe.",
+    speaker: "--Mark Twain"
+  },
+  {
+    statement: "Ask me no questions, and I’ll tell you no lies.",
+    speaker: "--Oliver Goldsmith"
+  },
+  {
+    statement: "There’s a fine line between genius and insanity. I have erased this line.",
+    speaker: "--Oscar Levant"
+  },
+  {
+    statement: "Always forgive your enemies – nothing annoys them so much.",
+    speaker: "--Oscar Wilde"
+  },
+  {
+    statement: "Of all the things I’ve lost I miss my mind the most.",
+    speaker: "--Ozzy Osbourne"
+  },
+  {
+    statement: "Life is a sexually transmitted disease.",
+    speaker: "--R. D. Laing"
+  },
+  {
+    statement: "If you have a secret, people will sit a little bit closer.",
+    speaker: "--Rob Corddry"
+  },
+  {
+    statement: "I have tried to know absolutely nothing about a great many things, and I have succeeded fairly well.",
+    speaker: "--Robert Benchley"
+  },
+  {
+    statement: "By working faithfully eight hours a day you may eventually get to be boss and work twelve hours a day.",
+    speaker: "--Robert Frost"
+  },
+  {
+    statement: "Have no fear of perfection. You’ll never reach it.",
+    speaker: "--Salvador Dali"
+  },
+  {
+    statement: "You cannot be anything if you want to be everything",
+    speaker: "--Solomon Schechter"
+  },
+  {
+    statement: "Crocodiles are easy. They try to kill and eat you. People are harder. Sometimes they pretend to be your friend first.",
+    speaker: "--Steve Irwin"
+  },
+  {
+    statement: "To steal ideas from one person is plagiarism; to steal from many is research.",
+    speaker: "--Steven Wright"
+  },
+  {
+    statement: "Opportunity is missed by most people because it is dressed in overalls and looks like work.",
+    speaker: "--Thomas A. Edison"
+  },
+  {
+    statement: "I don’t want to achieve immortality through my work. I want to achieve it through not dying.",
+    speaker: "--"
+  },
+  {
+    statement: "You’ve got to be very careful if you don’t know where you are going, because you might not get there.",
+    speaker: "--Yogi Berra"
+  },
 ];
 
 export default quoteList;
